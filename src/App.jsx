4aaf896d2d0d198ee09efbc2d5a1e8ff1bcdae66
Rahul Bairwa <rahullodiwal8@gmail.com>
2024-05-26 00:33:24 +0530

    Store, authSlice, and Provider SetUp Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import './App.css'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { Footer, Header } from './components'
+import { login, logout } from './store/features/authSlice'
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-function App() {
-  return (
-    <>
-    hekll
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then(() => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(()=>setLoading(false))
+  }, [])
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400 flex-wrap">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
